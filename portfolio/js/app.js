eebc3cb19e5f31e45de6fb80a93d83bf3e2383d8
Hossein <hosseinnoruzpur@outlook.com>
2023-12-11 16:36:36 +0330

    portfo_07

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -1,1 +1,12 @@
-// 
+let menu = document.querySelector(".menu");
+let hamburgBtn = document.querySelector(".menu-icon");
+let hamburgIcon = document.querySelector(".menu-icon i");
+hamburgBtn.addEventListener("click" , function(){
+  if(hamburgIcon.classList.contains("bi-list")){
+    menu.style.right = "-64px"
+    hamburgIcon.classList = "bi-x-lg"
+  }else{
+    menu.style.right = "-320px"
+    hamburgIcon.classList = "bi-list"
+  }
+})
